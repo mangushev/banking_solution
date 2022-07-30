4e3c8705980ab06d8f7c27f9636eba9ef9040878
rynad <rynad.mangushev@gmail.com>
2022-07-30 15:10:08 +0000

    changed to ddpg

---
+++ model.py
@@ -0,0 +1,162 @@
+
+#TODO:
+
+import numpy as np
+
+import tensorflow as tf
+import tensorflow_addons as tfa
+from tensorflow_probability import distributions as tfd
+
+import math
+
+class Dense(tf.Module):
+  def __init__(self, input_width, output_size, activation=None, stddev=1.0, name=''):
+    super(Dense, self).__init__()
+    self.w = tf.Variable(
+      tf.random.truncated_normal([input_width, output_size], stddev=stddev), name=name + '_w')
+    self.b = tf.Variable(tf.zeros([output_size]), name=name+'_b')
+    self.activation = activation
+    self.input_width = input_width
+    self.output_size = output_size
+  def __call__(self, x):
+    input_shape = x.shape
+
+    if len(input_shape) != 2 and len(input_shape) != 3:
+      raise ValueError("input shape rank {} shuld be 2 or 3".format(len(input_shape)))
+
+    if len(input_shape) == 3:
+      if self.input_width != input_shape[2]:
+        raise ValueError("widths do not match {} {}".format(self.input_width, input_shape[2]))
+      x = tf.reshape(x, [-1, self.input_width])
+    else:
+      if self.input_width != input_shape[1]:
+        raise ValueError("widths do not match {} {}".format(self.input_width, input_shape[1]))
+
+    y = tf.matmul(x, self.w) + self.b
+    if (self.activation is not None):
+      y = self.activation(y)
+
+    if len(input_shape) == 3:
+      return tf.reshape(y, [-1, input_shape[1], self.output_size])
+
+    return y
+
+#Policy network
+class Actor(tf.Module):
+  def __init__(self, num_accounts, num_features, safety_level, transfer_limit, hidden_size, activation=tf.nn.relu, dropout_prob=0.1):
+    super(Actor, self).__init__()
+    self.layer1 = Dense(num_features, hidden_size, activation=None)
+    self.layer2 = Dense(hidden_size, hidden_size, activation=None)
+    self.layer3 = Dense(hidden_size, 2, activation=None)
+    #self.mu = Dense(num_features, 1, activation=tf.math.tanh)
+    #self.log_var = Dense(num_features, 1, activation=tf.nn.sigmoid)
+    self.activation = activation
+    self.dropout_prob = dropout_prob
+    self.safety_level = safety_level
+    self.transfer_limit = transfer_limit
+  def __call__(self, state):
+    #[I, P] --> [I]
+    layer_output = self.layer1(state)
+    layer_output = tfa.layers.GroupNormalization(groups = 1)(layer_output) 
+    layer_output = self.activation(layer_output)
+    layer_output = tf.nn.dropout(layer_output, self.dropout_prob)
+
+    layer_output = self.layer2(layer_output)
+    layer_output = tfa.layers.GroupNormalization(groups = 1)(layer_output) 
+    layer_output = self.activation(layer_output)
+    layer_output = tf.nn.dropout(layer_output, self.dropout_prob)
+
+    layer_output = self.layer3(layer_output)
+    #layer_output = tfa.layers.GroupNormalization(groups = 1)(layer_output) 
+    #layer_output = self.activation(layer_output)
+    #layer_output = tf.nn.dropout(layer_output, self.dropout_prob)
+
+    # 0 <= u <= 1 eq 3
+    #I just limits to $10,000. I was experimenting with variety of other funtions like tanh, shifted positive and squeezed 
+    return tf.nn.sigmoid(layer_output)*self.transfer_limit
+
+  def random(self, state):
+    #(a, f) --> 2*(a)
+    mu_std = self(state)
+
+    epsilon = 0.0
+    #this might help exploring or use NoisyDense 
+    #epsilon = tf.random.normal([mu_std.shape[0]], mean=0.0, stddev=1.0, dtype=tf.dtypes.float32, name='epsilon')
+
+    return mu_std[:, 0] + epsilon
+          
+  def best(self, state):
+    #best means mean for continuous action space modeled with Normal distribution
+    return self(state)[:, 0]
+
+  def he(self, sales, x_he):
+    #(1), (a), (a) --> (a)
+    return tf.math.maximum(0, self.safety_level + sales - x_he)
+
+#Value network
+class Critic(tf.Module):
+  def __init__(self, num_features, hidden_size, activation=tf.nn.relu, dropout_prob=0.1):
+    super(Critic, self).__init__()
+    self.layer1 = Dense(num_features+1, hidden_size, activation=None)
+    self.layer2 = Dense(hidden_size, 1, activation=None)
+    self.activation = activation
+    self.dropout_prob = dropout_prob
+  def __call__(self, state, action):
+    #[I, P] --> [I]
+
+    s_a = tf.concat([state, tf.expand_dims(action, 1)], axis=1)
+
+    layer_output = self.layer1(s_a)
+    layer_output = tfa.layers.GroupNormalization(groups = 1)(layer_output) 
+    layer_output = self.activation(layer_output)
+    layer_output = tf.nn.dropout(layer_output, self.dropout_prob)
+
+    layer_output = self.layer2(layer_output)
+
+    #[I, 1] --> [I]
+    return tf.squeeze(layer_output, axis=-1, name='factor_squeeze')
+
+class Env(tf.Module):
+  def __init__(self, num_accounts, zero_balance, critical_balance, zero_weight, critical_weight, waste_level):
+    super(Env, self).__init__()
+    self.num_accounts = num_accounts
+    self.zero_balance = zero_balance
+    self.critical_balance = critical_balance
+    self.zero_weight = zero_weight
+    self.critical_weight = critical_weight
+    self.waste_level = waste_level
+
+  def reset(self, x):
+    self.x = x
+
+  def waste(self):
+    return self.waste_level * self.x
+
+  def __call__(self, u, sales):
+    #(a) + (a) --> (a)
+    x_u = self.x + u
+    #there can be more money on the account than 100k that is used to normalize data
+    #other project, inventory management, this is limited by shelf size
+    #x_u = tf.math.minimum(1, self.x + u)
+
+    overdraft = tf.math.minimum(0, x_u - sales)
+
+    #(a) - (a) --> (a)
+    self.x = tf.math.maximum(0, x_u - sales)
+
+    z = tf.cast(self.x < self.zero_balance, tf.float32)
+
+    critical = tf.cast(self.x < self.critical_balance, tf.float32)
+
+    q = tf.math.maximum(0, self.waste())
+
+    z = self.zero_weight * z
+
+    critical = self.critical_weight * critical
+
+    #(a), (a), (a), (a) --> (a)
+    r = tf.cast(1 - z - critical - q, tf.float32)
+    #some experimental reward function. Didn't work actually
+    #r = tf.cast(1- (tf.math.exp(-self.zero_weight*(self.x-overdraft))+q), tf.float32)
+
+    return self.x, overdraft, r, z, critical, q
---
+++ training.py
@@ -0,0 +1,292 @@
+import os
+import sys
+import argparse
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import tensorflow as tf
+
+from model import Actor, Critic, Env
+
+#tf.debugging.set_log_device_placement(True)
+
+import numpy as np
+np.set_printoptions(edgeitems=25, linewidth=10000, precision=12, suppress=True)
+
+FLAGS = None
+
+def trans_parser(serialized_example):
+  example = tf.io.parse_single_example(
+    serialized_example,
+    features={ 
+      "estimate": tf.io.FixedLenFeature([FLAGS.num_accounts, 1], tf.float32),
+      "actual": tf.io.FixedLenFeature([FLAGS.num_accounts, 1], tf.float32)
+    })
+
+  return example
+
+#predict means that we run sequence with somne random initial balance and expenditures for each time period
+def predict():
+  trans_dataset = tf.data.TFRecordDataset(FLAGS.predict_file)
+  trans_dataset = trans_dataset.map(trans_parser)
+  trans_dataset = trans_dataset.batch(FLAGS.batch_size, drop_remainder=True)
+
+  actor = Actor(FLAGS.num_accounts, FLAGS.num_features, FLAGS.safety_level, FLAGS.transfer_limit, FLAGS.hidden_size, activation=tf.nn.relu, dropout_prob=FLAGS.dropout_prob)
+
+  #0.1 is 10k when date normalized to +-100k min/max. Model makes average balance at about $3k, so that's why initial balance is set like this. If balance is high, model sets action zero until balance gets down
+  x = tf.random.uniform(shape=[FLAGS.num_accounts], minval=0.01, maxval=0.05, dtype=tf.dtypes.float32)
+  x_he = x
+
+  env = Env(FLAGS.num_accounts, FLAGS.zero_balance, FLAGS.critical_balance, FLAGS.zero_weight, FLAGS.critical_weight, FLAGS.waste)
+  env_he = Env(FLAGS.num_accounts, FLAGS.zero_balance, FLAGS.critical_balance, FLAGS.zero_weight, FLAGS.critical_weight, FLAGS.waste)
+
+  env.reset(x)
+  env_he.reset(x)
+
+  checkpoint = tf.train.Checkpoint(actor=actor)
+  checkpoint.restore(tf.train.latest_checkpoint(FLAGS.output_dir)).expect_partial()
+
+  with tf.io.gfile.GFile(FLAGS.output_file, "w") as writer:
+    for batch in trans_dataset:
+      for i in range(FLAGS.batch_size):
+        debit_estimate = batch["estimate"][i, :, 0]
+        debit = batch["actual"][i, : ,0]
+
+        q_estimate = x*FLAGS.waste
+        if (FLAGS.use_actual):
+          s = tf.transpose(tf.stack([x, debit, q_estimate], axis=0), perm=[1, 0])
+        else:
+          s = tf.transpose(tf.stack([x, debit_estimate, q_estimate], axis=0), perm=[1, 0])
+        u = actor.best(s)
+
+        x_prime, overdraft, r, z, critical, q = env(u, debit)
+
+        u_he = actor.he(debit, x_he)
+        x_prime_he, overdraft_he, r_he, z_he, critical_he, q_he = env_he(u_he, debit)
+
+        writer.write("stock:" + ','.join(  list(map(str,   x.numpy()    ))    ) + "\n")
+        writer.write("action:" + ','.join(  list(map(str,   u.numpy()    ))    ) + "\n")
+        writer.write("overdraft:" + ','.join(  list(map(str,   overdraft.numpy()    ))    ) + "\n")
+
+        writer.write("stock_he:" + ','.join(  list(map(str,   x_he.numpy()    ))    ) + "\n")
+        writer.write("action_he:" + ','.join(  list(map(str,   u_he.numpy()    ))    ) + "\n")
+        writer.write("overdraft_he:" + ','.join(  list(map(str,   overdraft_he.numpy()    ))    ) + "\n")
+
+        writer.write("estimate:" + ','.join(  list(map(str,   debit_estimate.numpy()    ))    ) + "\n")
+        writer.write("debit:" + ','.join(  list(map(str,   debit.numpy()    ))    ) + "\n")
+
+        x = x_prime
+        x_he = x_prime_he
+
+#training is both improving critic and actor and keeping balance in time!
+def train():
+  #   Dimentions LEGEND:
+  #   a - accounts
+  #   f - state features
+  #   t - timesteps
+
+  trans_dataset = tf.data.TFRecordDataset(FLAGS.train_file).window(FLAGS.batch_size, shift=FLAGS.batch_size-1, drop_remainder=True)
+
+  actor_learning_rate_fn = tf.optimizers.schedules.ExponentialDecay(FLAGS.actor_learning_rate, decay_steps=FLAGS.decay_steps, decay_rate=0.99, staircase=False)
+  critic_learning_rate_fn = tf.optimizers.schedules.ExponentialDecay(FLAGS.critic_learning_rate, decay_steps=FLAGS.decay_steps, decay_rate=0.99, staircase=False)
+
+  actor_optimizer = tf.optimizers.Adam(actor_learning_rate_fn)
+  critic_optimizer = tf.optimizers.Adam(critic_learning_rate_fn)
+
+  #Policy and Value networks with random weights 
+  actor = Actor(FLAGS.num_accounts, FLAGS.num_features, FLAGS.safety_level, FLAGS.transfer_limit, FLAGS.hidden_size, activation=tf.nn.relu, dropout_prob=FLAGS.dropout_prob)
+  critic = Critic(FLAGS.num_features, FLAGS.hidden_size, activation=tf.nn.relu, dropout_prob=FLAGS.dropout_prob)
+  env = Env(FLAGS.num_accounts, FLAGS.zero_balance, FLAGS.critical_balance, FLAGS.zero_weight, FLAGS.critical_weight, FLAGS.waste)
+
+  checkpoint_prefix = os.path.join(FLAGS.output_dir, "ckpt")
+  checkpoint = tf.train.Checkpoint(critic_optimizer=critic_optimizer, actor_optimizer=actor_optimizer, critic=critic, actor=actor)
+  status = checkpoint.restore(tf.train.latest_checkpoint(FLAGS.output_dir))
+
+  episode = -1
+  while (episode < FLAGS.train_episodes):
+    #random initial inventory, 5k - 15k balances.
+    x = tf.random.uniform(shape=[FLAGS.num_accounts], minval=0.05, maxval=0.15, dtype=tf.dtypes.float32)
+    env.reset(x)
+    q_estimate = x * FLAGS.waste
+
+    count = 0
+    for batch_dataset in trans_dataset:
+      with tf.GradientTape() as actor_tape, tf.GradientTape() as critic_tape:
+        experience_step = tf.constant(0)
+        experience_s = tf.TensorArray(size=FLAGS.batch_size, dtype=tf.float32, element_shape=tf.TensorShape([FLAGS.num_accounts, FLAGS.num_features]), name="experience_s")
+        experience_a = tf.TensorArray(size=FLAGS.batch_size, dtype=tf.float32, element_shape=tf.TensorShape([FLAGS.num_accounts]), name="experience_a")
+        experience_s_prime = tf.TensorArray(size=FLAGS.batch_size, dtype=tf.float32, element_shape=tf.TensorShape([FLAGS.num_accounts, FLAGS.num_features]), name="experience_s_prime")
+        experience_a_prime = tf.TensorArray(size=FLAGS.batch_size, dtype=tf.float32, element_shape=tf.TensorShape([FLAGS.num_accounts]), name="experience_a_prime")
+        experience_r = tf.TensorArray(size=FLAGS.batch_size, dtype=tf.float32, element_shape=tf.TensorShape([FLAGS.num_accounts]), name="experience_r_prime")
+        experience_z = tf.TensorArray(size=FLAGS.batch_size, dtype=tf.float32, element_shape=tf.TensorShape([FLAGS.num_accounts]), name="experience_z")
+        experience_critical = tf.TensorArray(size=FLAGS.batch_size, dtype=tf.float32, element_shape=tf.TensorShape([FLAGS.num_accounts]), name="experience_critical")
+        experience_q = tf.TensorArray(size=FLAGS.batch_size, dtype=tf.float32, element_shape=tf.TensorShape([FLAGS.num_accounts]), name="experience_q")
+        experience_debit = tf.TensorArray(size=FLAGS.batch_size, dtype=tf.float32, element_shape=tf.TensorShape([FLAGS.num_accounts]), name="experience_debit")
+
+        batch_iterator = batch_dataset.map(trans_parser)
+
+        debit_estimate = next(iter(batch_iterator))['estimate'][:, 0]
+        debit = next(iter(batch_iterator))['actual'][:, 0]
+
+        if (FLAGS.use_actual):
+          s = tf.transpose(tf.stack([x, debit, q_estimate], axis=0), perm=[1, 0])
+        else:
+          s = tf.transpose(tf.stack([x, debit_estimate, q_estimate], axis=0), perm=[1, 0])
+
+        for item in batch_iterator:
+          a = actor.random(s)
+
+          experience_s = experience_s.write(experience_step, s)
+
+          x, overdraft, r, z, critical, q = env(a, debit)
+
+          debit_estimate = item['estimate'][:, 0]
+          debit = item['actual'][:, 0]
+
+          #environment uses actual debit to calculate balance and waste. actor / critic are using estimates of debit and waste
+          q_estimate = x * FLAGS.waste
+
+          # if this is the last item in a batch, s will carry over to the next batch since batch shift is batch_size-1
+          #(a), (a), (a) --> (f, a) --> (a, f)
+          if (FLAGS.use_actual):
+            s = tf.transpose(tf.stack([x, debit, q_estimate], axis=0), perm=[1, 0])
+          else:
+            s = tf.transpose(tf.stack([x, debit_estimate, q_estimate], axis=0), perm=[1, 0])
+
+          a_prime = actor.random(s)
+
+          experience_a = experience_a.write(experience_step, a)
+          experience_s_prime = experience_s_prime.write(experience_step, s)
+          experience_a_prime = experience_a_prime.write(experience_step, a_prime)
+          experience_r = experience_r.write(experience_step, r)
+          experience_z = experience_z.write(experience_step, z)
+          experience_critical = experience_critical.write(experience_step, critical)
+          experience_q = experience_q.write(experience_step, q_estimate)
+          experience_debit = experience_debit.write(experience_step, debit)
+
+          experience_step = experience_step + 1
+
+        #(t, a, f) --> (t*a, f)
+        s_batch = tf.reshape(experience_s.stack()[:experience_step, :, :], [-1, FLAGS.num_features])
+        x_batch = tf.reshape(experience_s.stack()[:experience_step, :, 0], [-1])
+        sal_bat = tf.reshape(experience_s.stack()[:experience_step, :, 1], [-1])
+        a_batch = tf.reshape(experience_a.stack()[:experience_step, :], [-1])
+        s_prime_batch = tf.reshape(experience_s_prime.stack()[:experience_step, :, :], [-1, FLAGS.num_features])
+        a_prime_batch = tf.reshape(experience_a_prime.stack()[:experience_step, :], [-1])
+        r_batch = tf.reshape(experience_r.stack()[:experience_step, :], [-1])
+        z_batch = tf.reshape(experience_z.stack()[:experience_step, :], [-1])
+        critical_batch = tf.reshape(experience_critical.stack()[:experience_step, :], [-1])
+        q_batch = tf.reshape(experience_q.stack()[:experience_step, :], [-1])
+        debit_batch = tf.reshape(experience_debit.stack()[:experience_step, :], [-1])
+
+        tf.print("rewards:", actor_optimizer.iterations, episode, tf.reduce_mean(r_batch, keepdims=False), output_stream=sys.stderr, summarize=-1)
+        tf.print("overdrafts:", actor_optimizer.iterations, episode, tf.reduce_mean(z_batch, keepdims=False), output_stream=sys.stderr, summarize=-1)
+        tf.print("critical:", actor_optimizer.iterations, episode, tf.reduce_mean(critical_batch, keepdims=False), output_stream=sys.stderr, summarize=-1)
+        tf.print("waste:", actor_optimizer.iterations, episode, tf.reduce_mean(q_batch, keepdims=False), output_stream=sys.stderr, summarize=-1)
+
+        tf.print("x    :", actor_optimizer.iterations, episode, tf.reduce_mean(x_batch, keepdims=False), output_stream=sys.stderr, summarize=-1)
+        tf.print("a    :", actor_optimizer.iterations, episode, tf.reduce_mean(a_batch, keepdims=False), output_stream=sys.stderr, summarize=-1)
+        tf.print("estimate:", actor_optimizer.iterations, episode, tf.reduce_mean(sal_bat, keepdims=False), output_stream=sys.stderr, summarize=-1)
+        tf.print("debit:", actor_optimizer.iterations, episode, tf.reduce_mean(debit_batch, keepdims=False), output_stream=sys.stderr, summarize=-1)
+
+        #(t*a, f) --> (t*a)
+        v = critic(s_batch, a_batch)
+
+        #(t*a, f) --> (t*a)
+        v_prime = critic(s_prime_batch, a_prime_batch)
+
+        y = r_batch + FLAGS.gamma*v_prime
+
+        #(t*a, t*a, t*a) --> (t*a)
+        delta = y - v
+        tf.print("delta:", actor_optimizer.iterations, episode, tf.reduce_mean(delta, keepdims=False), output_stream=sys.stderr, summarize=-1)
+
+        #(t*a) --> (1)
+        critic_loss = tf.reduce_mean(tf.math.square(delta), keepdims=False)
+        tf.print("critic loss:", actor_optimizer.iterations, episode, critic_loss, output_stream=sys.stderr, summarize=-1)
+
+        delta_stopped = tf.stop_gradient(delta)
+
+        #(t*a), (t*a), (1) --> (1), (1) --> (1)
+        actor_loss = -tf.reduce_mean(v, keepdims=False)
+
+        tf.print("actor loss:", actor_optimizer.iterations, episode, actor_loss, actor_optimizer.lr(actor_optimizer.iterations), output_stream=sys.stderr, summarize=-1)
+
+      actor_gradients = actor_tape.gradient(actor_loss, actor.variables)
+      critic_gradients = critic_tape.gradient(critic_loss, critic.variables)
+      count = count + 1
+
+      actor_optimizer.apply_gradients(zip(actor_gradients, actor.variables))
+      critic_optimizer.apply_gradients(zip(critic_gradients, critic.variables))
+
+    episode = int(actor_optimizer.iterations / count)
+
+    if (episode + 1) % FLAGS.save_episodes == 0:
+      checkpoint.save(file_prefix=checkpoint_prefix)
+
+def main():
+  if FLAGS.action == 'TRAIN':
+    with tf.device('/cpu:0'):
+      train()
+  elif FLAGS.action == 'PREDICT':
+    with tf.device('/cpu:0'):
+      predict()
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--output_dir', type=str, default='checkpoints',
+            help='Model directrory in google storage.')
+    parser.add_argument('--train_file', type=str, default='data/train.tfrecords',
+            help='Train file location in google storage.')
+    parser.add_argument('--predict_file', type=str, default='data/test.tfrecords',
+            help='Predict/Test file location in google storage.')
+    parser.add_argument('--output_file', type=str, default='./output.csv',
+            help='Prediction output.')
+    parser.add_argument('--dropout_prob', type=float, default=0.1,
+            help='This used for all dropouts.')
+    parser.add_argument('--train_episodes', type=int, default=600,
+            help='How many times to run scenarious.')
+    parser.add_argument('--save_episodes', type=int, default=10,
+            help='Save every N episodes.')
+    parser.add_argument('--num_accounts', type=int, default=4997,
+            help='How many customer accounts.')
+    parser.add_argument('--num_features', type=int, default=3,
+            help='How many features in Critic/Actor network.')
+    parser.add_argument('--hidden_size', type=int, default=96,
+            help='Actor and Critic layers hidden size.')
+    parser.add_argument('--gamma', type=float, default=0.99,
+            help='Discount in future rewards.')
+    parser.add_argument('--critical_balance', type=float, default=0.005,
+            help='Critical balance comes earlier than overdraft as an additional warning to the model.')
+    parser.add_argument('--zero_weight', type=float, default=0.1,
+            help='Coefficient applied to number of overdrafts in reward formula.')
+    parser.add_argument('--critical_weight', type=float, default=0.1,
+            help='Coefficient applied to number of critical balance breach in reward formula.')
+    parser.add_argument('--waste', type=float, default=32,
+            help='Waste is an opportunity loss for customer. It signals to the model to avoid piling monet to the account.')
+    parser.add_argument('--safety_level', type=float, default=0.05,
+            help='Balance safety level for heuristic computation.')
+    parser.add_argument('--transfer_limit', type=float, default=0.1,
+            help='Maximum action - transfer amount. 0.1 is 10k.')
+    parser.add_argument('--use_actual', default=False, action='store_true',
+            help='Use debit estimates values or use actual (crystal ball) values.')
+    parser.add_argument('--actor_learning_rate', type=float, default=0.001,
+            help='Optimizer learning rate for Actor.')
+    parser.add_argument('--critic_learning_rate', type=float, default=0.001,
+            help='Optimizer learning rate for Critic.')
+    parser.add_argument('--logging', default='INFO', choices=['DEBUG','INFO','WARNING','ERROR','CRITICAL'],
+            help='Enable excessive variables screen outputs.')
+    parser.add_argument('--decay_steps', type=int, default=400,
+            help='Exponential decay parameter.')
+    parser.add_argument('--zero_balance', type=float, default=1e-5,
+            help='Consider as zero balance if less than that.')
+    parser.add_argument('--batch_size', type=int, default=2,
+            help='Batch size.')
+    parser.add_argument('--action', default='PREDICT', choices=['TRAIN','PREDICT'],
+            help='An action to execure.')
+
+    FLAGS, unparsed = parser.parse_known_args()
+
+    main()
